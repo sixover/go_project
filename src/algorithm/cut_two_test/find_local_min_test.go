da9cb3e0f593dd7beb97036c9e7512f3fe818ee8
sixover <2565927503@qq.com>
2022-09-27 21:03:35 +0800

    1

---
+++ src/algorithm/cut_two_test/find_local_min_test.go
@@ -0,0 +1,1 @@
+package cut_two
--- src/algorithm/cut_two_test/find_nearest_num_test.go
+++ src/algorithm/cut_two_test/find_nearest_num_test.go
@@ -12,5 +12,29 @@ func TestNearestFind(t *testing.T) {
 }
 
 func NearestFinder(arr []int, num int) int {
-
+	if len(arr) == 0 {
+		return -1
+	}
+	if arr[0] > num {
+		return -1
+	}
+	if arr[len(arr)-1] < num {
+		return -1
+	}
+	l := 0
+	r := len(arr) - 1
+	index := -1
+	for {
+		mid := l + (r-l)/2
+		if arr[mid] >= num {
+			index = mid
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+		if r < l {
+			break
+		}
+	}
+	return index
 }
