069ec4d27e3780916f10e0523eccb70a686e0d15
sixover <2565927503@qq.com>
2022-10-25 23:35:06 +0800

    1

---
+++ src/NAT-penetration/tunnel_greater_test.go
@@ -0,0 +1,156 @@
+package NAT_penetration
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+const (
+	controlerAddr     = "0.0.0.0:22222"
+	requestServerAddr = "0.0.0.0:22223"
+)
+
+var wg sync.WaitGroup
+var clientConn *net.TCPConn
+
+func controler() {
+	log.Println("contoler start!")
+	tcpListener, err := createListener(controlerAddr)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		clientConn, err = tcpListener.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+		//go keepAlive(clientConn)
+	}
+}
+
+func requestServer() {
+	log.Println("requestServer start!")
+	tcpListener, err := createListener(requestServerAddr)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		tcpConn, err := tcpListener.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			i, err := io.Copy(clientConn, tcpConn)
+			if err != nil {
+				log.Println(i)
+				panic(err)
+			}
+			log.Println("copy tcpConn to clientConn")
+		}()
+		go func() {
+			i, err := io.Copy(tcpConn, clientConn)
+			if err != nil {
+				log.Println(i)
+				panic(err)
+			}
+			log.Println("copy clientConn to tcpConn")
+		}()
+	}
+}
+
+func createListener(addr string) (*net.TCPListener, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+	return tcpListener, nil
+}
+
+func createConn(addr string) (*net.TCPConn, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+	return tcpConn, nil
+}
+
+func keepAlive(conn *net.TCPConn) {
+	for {
+		_, err := conn.Write([]byte("keep alive\n"))
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func TestControlCenter(t *testing.T) {
+	wg.Add(1)
+	go controler()
+	go requestServer()
+	wg.Wait()
+}
+
+func TestNAT(t *testing.T) {
+	tcpConn, err := createConn(controlerAddr)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		readString, err := bufio.NewReader(tcpConn).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		log.Println(readString)
+		if readString == "keep alive\n" {
+			continue
+		}
+		_, err = tcpConn.Write([]byte(readString))
+		if err != nil {
+			panic(err)
+		}
+		break
+	}
+	tcpConn.Close()
+}
+
+func TestUserClient(t *testing.T) {
+	tcpConn, err := createConn(requestServerAddr)
+	if err != nil {
+		panic(err)
+	}
+	_, err = tcpConn.Write([]byte("i am net package\n"))
+	if err != nil {
+		panic(err)
+	}
+	respFromServer := make([]byte, 0)
+	for {
+		var buffer [bufferSize]byte
+		n, err := tcpConn.Read(buffer[:])
+		if err != nil {
+			t.Fatal(err)
+		}
+		respFromServer = append(respFromServer, buffer[:n]...)
+		if n < bufferSize {
+			break
+		}
+	}
+	err = tcpConn.Close()
+	if err != nil {
+		panic(err)
+	}
+	log.Println(respFromServer)
+}
--- src/NAT-penetration/tunnel_test.go
+++ src/NAT-penetration/tunnel_test.go
@@ -1,1 +1,171 @@
 package NAT_penetration
+
+import (
+	"io"
+	"net"
+	"strconv"
+	"testing"
+)
+
+const (
+	serverAddr = "0.0.0.0:22222"
+	tunnelAddr = "0.0.0.0:22223"
+)
+
+func TestServer(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for {
+		tcpConn, err := tcpListener.AcceptTCP()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resp := make([]byte, 0)
+		for {
+			var buffer [bufferSize]byte
+			n, err := tcpConn.Read(buffer[:])
+			if err != nil {
+				t.Fatal(err)
+			}
+			resp = append(resp, buffer[:n]...)
+			if n < bufferSize {
+				break
+			}
+		}
+		t.Log(resp)
+		i, err := strconv.Atoi(string(resp))
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = tcpConn.Write([]byte(strconv.Itoa(i + 2)))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestClient(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", tunnelAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//发送数据
+	_, err = tcpConn.Write([]byte(strconv.Itoa(2300)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//接收数据
+	resp := make([]byte, 0)
+	for {
+		var buffer [bufferSize]byte
+		n, err := tcpConn.Read(buffer[:])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resp = append(resp, buffer[:n]...)
+		if n < bufferSize {
+			break
+		}
+	}
+	t.Log(string(resp))
+	err = tcpConn.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestTunnel(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", tunnelAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for {
+		tcpConn, err := listener.AcceptTCP()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		//respFromClient :=make([]byte,0)
+		//for{
+		//	var buffer [bufferSize]byte
+		//	n, err := tcpConn.Read(buffer[:])
+		//	if err != nil {
+		//		t.Fatal(err)
+		//	}
+		//	respFromClient =append(respFromClient,buffer[:n]...)
+		//	if n<bufferSize{
+		//		break
+		//	}
+		//}
+		//t.Log(respFromClient)
+
+		serverTcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		serverTcpConn, err := net.DialTCP("tcp", nil, serverTcpAddr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		//_, err = serverTcpConn.Write(respFromClient)
+		//if err != nil {
+		//	t.Fatal(err)
+		//}
+		//respFromServer:=make([]byte,0)
+		//for{
+		//	var buffer [bufferSize]byte
+		//	n, err := serverTcpConn.Read(buffer[:])
+		//	if err != nil {
+		//		t.Fatal(err)
+		//	}
+		//	respFromServer=append(respFromServer,buffer[:n]...)
+		//	if n < bufferSize{
+		//		break
+		//	}
+		//}
+		//t.Log(respFromServer)
+		//
+		//_, err = tcpConn.Write(respFromServer)
+		//if err != nil {
+		//	t.Fatal(err)
+		//}
+		go func() {
+			_, err := io.Copy(serverTcpConn, tcpConn)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+		}()
+
+		go func() {
+			_, err := io.Copy(tcpConn, serverTcpConn)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+		}()
+	}
+}
